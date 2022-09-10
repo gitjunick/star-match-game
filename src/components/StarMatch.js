3395f3f6bb04f4891a3f58dec761ba974c765db8
Junick Marco Aquino <aquijun@mliph.manulife.com.ph>
2022-09-10 14:35:11 +0800

    Display number of stars and buttons with Map.

--- src/components/StarMatch.js
+++ src/components/StarMatch.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { utils } from "../utils/utils";
 
 const StarMatch = () => {
+    const stars = 6;
+    const buttons = 9;
+
     return (
         <div className="game">
         <div className="help">
@@ -8,26 +12,14 @@ const StarMatch = () => {
         </div>
         <div className="body">
             <div className="left">
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
+                {utils.range(1, stars).map(starId => {
+                    return <div key={starId} className="star" />
+                })}
             </div>
             <div className="right">
-            <button className="number">1</button>
-            <button className="number">2</button>
-            <button className="number">3</button>
-            <button className="number">4</button>
-            <button className="number">5</button>
-            <button className="number">6</button>
-            <button className="number">7</button>
-            <button className="number">8</button>
-            <button className="number">9</button>
+                {utils.range(1, buttons).map(number => {
+                    return <button key={number} className="number">{number}</button>
+                })}
             </div>
         </div>
         <div className="timer">Time Remaining: 10</div>
